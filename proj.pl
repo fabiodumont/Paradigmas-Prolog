e02380aad6e1c4ac974a5a041edf3eae47c1e984
Fabio Luis Dumont <32838165+fabiodumont@users.noreply.github.com>
2019-05-31 09:02:25 -0300

    Add files via upload

---
+++ proj.pl
@@ -0,0 +1,27 @@
+vôo(origem,destino,código,partida,(dia_chegada,horario_chegada), número_de_escalas,companhia,[dias]).
+
+vôo(sao_paulo,mexico,gl0,7:30,(mesmo,17:30),0,gol,[qua,sex,sab,dom]).
+vôo(sao_paulo,nova_york,g11,6:20,(mesmo,16:20),0,gol,[ter,sex,dom]).
+vôo(sao_paulo,lisboa,g12,15:30,(dia_seguinte,5:20),0,gol,[seg,ter,sex,dom]).
+vôo(sao_paulo,madrid,gl3,8:25,(mesmo,20:25),0,gol,[qua,dom]).
+vôo(sao_paulo,londres,gl4,8:25,(dia_seguinte,8:25),1,gol,[seg,qui]).
+vôo(sao_paulo,paris,gl5,18:55,(dia_seguinte,7:35),0,gol,[ter,sex]).
+vôo(mexico,nova_york,il0,10:00,(mesmo,15:00),0,interjet,[seg,qua,sex,dom]).
+vôo(mexico,madrid,axl5,4:00,(mesmo,16:00),0,aeromexico,[seg,qua,sex,dom]).
+vôo(nova_york,londres,aa23,22:30,(dia_seguinte,6:30),0,american_airlines,[seg,qua,sex,dom]).
+vôo(londres,lisboa,bal0,6:00,(mesmo,8:35),0,british_airways,[seg,ter,qua,qui,sex,sab,dom]).
+vôo(londres,paris,bal4,7:20,(mesmo,9:35),0,british_airways,[seg,ter,qua,qui,sex,sab,dom]).
+vôo(londres,estocolmo,bal8,7:05,(mesmo,10:35),0,british_airways,[seg,ter,qua,quin,sex,sab,dom]).
+vôo(madrid,paris,afl0,6:00,(mesmo,8:05),0,air_france,[seg,ter,qua,quin,sex,sab,dom]).
+vôo(madrid,roma,ael0,7:05,(mesmo,9:30),0,air_europa,[seg,ter,qua,quin,sex,sab,dom]).
+vôo(madrid,frankfurt,ab46,7:35,(mesmo,9:35),0,air_berlin,[seg,ter,qua,quin,sex,sab,dom]).
+
+vôo(frankfurt,estocolmo,ltl0,10:20,(mesmo,12:25),0,lufthansa,[seg,ter,qua,quin,sex,sab,dom]).
+
+vôo(frankfurt,roma,ltl0,10:45,(mesmo,12:35),0,lufthansa,[seg,ter,qua,quin,sex,sab,dom]).
+
+
+
+
+
+
